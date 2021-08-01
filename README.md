6475b63215c58de0e8e7d66b3a50f5599c1bdbfd
will <william.yizima@hotmail.com>
2021-08-01 10:59:18 -0300

    insert params in request

--- README.md
+++ README.md
@@ -17,4 +17,5 @@ uvicorn main:app --reload
 - query q
 - http://127.0.0.1:8000/docs
 - http://127.0.0.1:8000/redoc
+- http://127.0.0.1:8000/openapi.json
 - create docs
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from enum import Enum
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -10,13 +11,29 @@ class Item(BaseModel):
     price: float
     is_offer: Optional[bool] = None
 
-@app.get('/')
-async def read_root():
-    return {"Hello":'World'}
+class ModelName(str, Enum):
+    rafael = "rafael"
+    luan = "luan"
 
-@app.get('/items/{item_id}')
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id":item_id, "q":q}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/")
+async def read_root():
+    return {"Hello":"World"}
+
+#this example skip and limit is not required and default 0
+@app.get("/items/")
+async def read_items(skip: int=0, limit: int=0):
+    return fake_items_db[skip: skip+limit]
+
+@app.get("/need/{item_id}")
+async def need_items(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id":item_id, "q":q}
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
@@ -24,3 +41,16 @@ async def update_item(item_id: int, item: Item):
             "item_price":item.price,
             "is_offer":item.is_offer,
             "item_id":item_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if(model_name == ModelName.luan):
+        return {"model_name":model_name, "message":"olha seu nome é luan!"}
+    if(model_name.value == ModelName.rafael):
+        return {"model_name":model_name, "message": "olha seu nome é rafael"}
+    
+    return {"model_name":"não achei", "message":"não achei nada"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
