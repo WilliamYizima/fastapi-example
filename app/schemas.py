5234c809b3f69948fcadd2a73dd5839da6fc991c
will <william.yizima@hotmail.com>
2021-08-01 14:27:40 -0300

    orm-test sqlite and csv data

---
+++ app/schemas.py
@@ -0,0 +1,13 @@
+from datetime import date
+from pydantic import BaseModel
+
+class Record(BaseModel):
+    id: int
+    date: date
+    country: str
+    cases: int
+    deaths: int
+    recoveries: int
+
+    class Config:
+        orm_mode = True
